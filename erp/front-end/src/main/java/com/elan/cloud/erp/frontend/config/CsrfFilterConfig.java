ce22e2fbfe4e9f1f03f7a16734c43539ec7a4530
lyswin2002 <lyswin2002@163.com>
2018-05-15 10:37:48 +0800

    添加过滤器（log、csrf）

---
+++ erp/front-end/src/main/java/com/elan/cloud/erp/frontend/config/CsrfFilterConfig.java
@@ -0,0 +1,40 @@
+package com.elan.cloud.erp.frontend.config;
+
+import com.elan.common.filter.CsrfFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CsrfFilterConfig {
+    private Logger logger = LoggerFactory.getLogger(CsrfFilterConfig.class);
+
+    @Value("${security.csrf.isOpen:true}")
+    private String isOpen;
+
+    //排除不拦截的URL
+    @Value("${security.csrf.excludes:/css,/js,/lib,/images,*.ico}")
+    private String excludes;
+
+
+    @Bean
+    public FilterRegistrationBean csrfFilterRegistrationBean() {
+        logger.info("CsrfFilter is running -----------------------------");
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new CsrfFilter());
+        filterRegistrationBean.setOrder(3);
+        filterRegistrationBean.setEnabled(true);
+        filterRegistrationBean.addUrlPatterns("/*");
+        Map<String, String> initParameters = new HashMap();
+        initParameters.put("excludes", excludes);
+        initParameters.put("isOpen", isOpen);
+        filterRegistrationBean.setInitParameters(initParameters);
+        return filterRegistrationBean;
+    }
+}
